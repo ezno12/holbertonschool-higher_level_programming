ddb79334a5d2a3075183963ea6a6ccd3dbc19100
tennin12 <bassembenyhia12@gmail.com>
2021-04-27 21:30:58 +0100

    f8 added

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if (ord(c) >= 65 and ord(c) <= 90):
-        return True
-    else:
-        return False
+def uppercase(str):
+    for c in str:
+        if (ord(str[c]) >= 97 and ord(str[c]) <= 122):
+           c = ord(str[c]-32)
+        print("{}".format(c), end="")
+print("")
