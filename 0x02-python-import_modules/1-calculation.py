99c3f69871d05917e49e6ba3e00409fbfe189f5e
tennin12 <bassembenyhia12@gmail.com>
2021-04-29 16:45:27 +0100

    new update for f1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,14 @@
+#!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-"""main file to make math operations.
 
-    arg:
-       a: first int
-       b: second int
-"""
+
 def main():
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, add(a, b)))
-    print("{} * {} = {}".format(a, b, add(a, b)))
-    print("{} / {} = {}".format(a, b, add(a, b)))
-
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     main()
