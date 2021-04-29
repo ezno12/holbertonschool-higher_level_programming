f6be2c5fae01c8c04933447dae3f21ed71aca124
tennin12 <bassembenyhia12@gmail.com>
2021-04-29 16:35:15 +0100

    f1 added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import add_0
 
-a = 1
-b = 2
-x = add_0.add(a,b)
-print("{:d}".format(x))
+def main():
+    a = 1
+    b = 2
+    x = add_0.add(a,b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
