f7b7157d26f656934daa9b20c3cf12f0054ffd56
tennin12 <bassembenyhia12@gmail.com>
2021-04-29 16:46:38 +0100

    f0 corrected & fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
-    x = add_0.add(a,b)
     print("{} + {} = {}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
