7fe828bcecc2de374fd97b3fc67499ea3f9c0936
tennin12 <bassembenyhia12@gmail.com>
2021-05-06 18:19:41 +0100

    f1 added

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list = []
+    for i in my_list:
+        if i == search:
+            list.append(replace)
+        else:
+            list.append(i)
+    return (list)
