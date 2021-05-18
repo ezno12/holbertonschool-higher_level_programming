1cfa970ff5fd05d58a287ec1288d37781e45d624
tennin12 <bassembenyhia12@gmail.com>
2021-05-18 22:12:15 +0100

    f2 added, square class created with opitional instantition

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Modul Sqaure """
+
+
+class Square:
+    """ Square class defined by geometric shap
+        Attributes:
+            size : int
+    """
+    def __init__(self, size=0):
+        """
+        Initialize method
+        Args:
+            size : int
+        Raises:
+            TypeError: if size is not int
+            ValueError: size less than 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
