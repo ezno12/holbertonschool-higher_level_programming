d7d1e13887ea1dc4d6a24ece50238c0904840f30
tennin12 <bassembenyhia12@gmail.com>
2021-05-18 22:08:29 +0100

    f1 added, class square with initial method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Sqaure Module
+"""
+
+
+
+class Square:
+    """ Square class defined by geomtric shap
+
+    Attributes:
+        size(int)
+
+
+    """
+    
+
+    def __init__(self, size):
+        """build method
+           
+           arg:
+              size: int
+        """
+
+        self.__size = size
