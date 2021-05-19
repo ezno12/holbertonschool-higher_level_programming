c6d7ff7cd7fe8ee303512a7cc745d1eb0a5469c4
tennin12 <bassembenyhia12@gmail.com>
2021-05-19 18:11:34 +0100

    f6 added

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    function that raise given message
+    """
+    try
+        raise NameError(message)
+    except:
+        raise
