6e9a95fcca5fb20263eaf34af85ff4da6db550b5
tennin12 <bassembenyhia12@gmail.com>
2021-05-21 18:56:34 +0100

    f4 added

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+this module provide function print (#) square of integers.
+args is int
+"""
+
+def print_square(size):
+    """
+    print_square - print (#) square of integers.
+    :param size: int
+    :return: printed square (#)
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end ="")
+        print()
