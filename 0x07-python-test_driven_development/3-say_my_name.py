6c79ed7b351d7070f6b248ec82e01953ef0d1ba5
tennin12 <bassembenyhia12@gmail.com>
2021-05-21 18:13:49 +0100

    f3 added

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+this module pprint two argement most be string.
+args are two string.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    say_m_name - print the first name and last name
+
+    :param first_name: string
+    :param last_name: strin
+
+    raise:
+         TypeError: in case args not strings
+
+    :return:
+    """
+
+    if first_name is None or not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if last_name not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
