60551b292f4113cd279fe1287eb12970137b6146
tennin12 <bassembenyhia12@gmail.com>
2021-05-22 15:26:23 +0100

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,21 +13,17 @@ def text_indentation(text):
     raise:
         TypeError: if text not string
 
-
     :return: text
     """
 
     if type(text) is not str:
         raise TypeError("text must be a string")
-
     a = 0
     while a < len(text):
-
         if text[a] in [':', '.', '?']:
             print(text[a])
             print()
             a += 1
-
         else:
             print(text[a], end='')
         a += 1
