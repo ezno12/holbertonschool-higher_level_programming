68186cb65ad7c2ead06bd375b0df8abb159a4428
tennin12 <bassembenyhia12@gmail.com>
2021-06-03 15:18:25 +0100

    f0 added

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+Module: 0-read-file
+
+"""
+
+def read_file(filename=""):
+    """
+
+    Read_file - function to read a text file and lrint stdout
+    
+    """
+    if filename is None:
+        return None
+
+    with open(filename, encoding="utf-8", mode="r") as file:
+        text = file.read()
+
+    print("{}".format(text), end="")
