e60eebbbb0f23fda2b3d0f13028e31f31ec18452
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 16:16:37 +0100

    f1 fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,6 +10,6 @@ def write_file(filename="", text=""):
 
     """
 
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
     return len(text)
