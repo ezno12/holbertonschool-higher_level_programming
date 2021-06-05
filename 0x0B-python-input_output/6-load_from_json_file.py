35996257fbbc80fda3893f8103524315b83e1769
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 17:21:24 +0100

    f6 added

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     creat object from JSON file
     """
-    with open(filename, "w", encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         return json.load(f)
