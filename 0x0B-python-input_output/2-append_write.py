cdba8b6ad2aba934675656b4e6603d72d5376d8c
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 15:55:13 +0100

    f2 added

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Modul: 2-append_write
+"""
+
+def append_write(filename="", text=""):
+    """
+    append a string to the end of a text file
+    """
+
+    with open(filename, "a", encoding="uft-8") as file:
+        file.write(text)
+        return len(text)
