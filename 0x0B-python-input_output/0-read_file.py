31ac66dbc9d0410e2c0904a7d39436acb2cbc533
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 15:44:15 +0100

    f0 update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-
 Module: 0-read-file
-
 """
 
 def read_file(filename=""):
     """
-
     Read_file - function to read a text file and lrint stdout
-    
     """
     if filename is None:
         return None
@@ -17,4 +13,4 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8", mode="r") as file:
         text = file.read()
 
-    print("{}".format(text), end="")
+    print(text, end="")
