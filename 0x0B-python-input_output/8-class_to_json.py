152083f7299b22db144152e8ca5deb7566c44ceb
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 17:30:47 +0100

    f8 added

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Modul: 8-class_to_json
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure.
+    """
+    return(obj.__dict__)
