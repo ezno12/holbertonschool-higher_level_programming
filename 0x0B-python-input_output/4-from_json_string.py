3ea995d5787f4189b273258ba60483cff13f9933
tennin12 <bassembenyhia12@gmail.com>
2021-06-05 17:03:02 +0100

    f4 added

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Modul: 4-from_json_string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    return an object from json
+    """
+    return json.loads(my_str)
