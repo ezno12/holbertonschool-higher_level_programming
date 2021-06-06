e72de3a096cd9cb0545596d1383d833d7d0b623e
tennin12 <bassembenyhia12@gmail.com>
2021-06-06 15:14:22 +0100

    f2 update it

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,8 +3,12 @@
 modul: 2-is_same_class
 """
 
+
 def is_same_class(obj, a_class):
     """
     determines if 'obj' is exactly an instance of 'a_class'
+    arg:
+        obj: to check it 
+        a_class: class to check on it
     """
     return issubclass(a_class, type(obj))
