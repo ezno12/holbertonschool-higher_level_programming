2614f37ba782f65b6f5aa56f7f142c1389bec444
tennin12 <bassembenyhia12@gmail.com>
2021-06-06 16:14:55 +0100

    f6 added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    module: 6-base_geometry
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """
+        public instance
+        raise:
+             Expection : area not used
+
+        """
+
+        raise Exception("area() is not implemented")
