dcbc3066e8ba48e1e0b8abb1b26947260b23d534
tennin12 <bassembenyhia12@gmail.com>
2021-06-19 11:15:45 +0100

    value erreur fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,10 +41,10 @@ class Rectangle:
             TypeError: When value is not int
             ValueError: When value is less than 0
         """
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -64,8 +64,8 @@ class Rectangle:
             TypeError: When value is not int
             ValueError: When value is less than 0
         """
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
