c957c7342e6c705f019cbc68353da0604f8c5196
tennin12 <bassembenyhia12@gmail.com>
2021-08-05 14:35:17 -0400

    f1 : semistd fixed

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
- // class rectangle contain height & width
+// class rectangle contain height & width
 module.exports = class Rectangle {
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
