3b1a61971288cdef0b79de94c9e79b5dc0f2da84
tennin12 <bassembenyhia12@gmail.com>
2021-08-19 22:23:01 +0100

    f6 fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,16 +5,16 @@ space complexity: O(1)
 """
 
 
-def find_peak(list):
-    start, end = 0, len(list) - 1
+def find_peak(list_of_integers):
+    start, end = 0, len(list_of_integers) - 1
     while start < end:
         mid = start + (end - start) // 2
 
         # Check if it's a peak
-        if list[mid] > list[mid - 1] and list[mid] > list[mid + 1]:
-            return list[mid]
-        if list[mid - 1] > list[mid + 1]:
+        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        if list_of_integers[mid - 1] > list_of_integers[mid + 1]:
             end = mid
         else:
             start = mid + 1
-        return list[start]
+        return list_of_integers[start]
