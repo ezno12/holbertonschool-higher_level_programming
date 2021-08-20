1450b8ac6400acb4bf799c64e570301dc7ad0dae
tennin12 <bassembenyhia12@gmail.com>
2021-08-20 23:26:07 +0100

    f0 added

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+fetches https://intranet.hbtn.io/status
+"""
+
+
+def main():
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reponse:
+        html = reponse.read()
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+ 
+if __name__ == '__main__':
+    main()
