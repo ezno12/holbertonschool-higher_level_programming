cfcd96e25d9dae2a930cda9bd6cf2a8c98d3a323
tennin12 <bassembenyhia12@gmail.com>
2021-08-21 00:52:43 +0100

    f5 added

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+
+
+def main():
+    """ Entry point """
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    r = requests.get(url)
+
+    print(r.headers.get('X-Request-Id'))
+
+if __name__ == '__main__':
+    main()
