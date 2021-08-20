a8883a366275dd6372444d7eea15c72f86263ad6
tennin12 <bassembenyhia12@gmail.com>
2021-08-21 00:55:00 +0100

    f6 added

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a
+parameter, and finally displays the body of the response.
+"""
+
+
+def main():
+    """ Entry point """
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    payload = {
+        'email': email
+    }
+
+    r = requests.post(url, data=payload)
+    print(r.text)
+
+if __name__ == '__main__':
+    main()
