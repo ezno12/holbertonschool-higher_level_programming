31760d0675469df5f7591090ef3c5eae9ca59005
tennin12 <bassembenyhia12@gmail.com>
2021-08-21 00:51:27 +0100

    f4 added

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+fetches https://intranet.hbtn.io/status
+"""
+
+
+def main():
+    """ Entry point """
+    import requests
+
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
+
+if __name__ == '__main__':
+    main()
