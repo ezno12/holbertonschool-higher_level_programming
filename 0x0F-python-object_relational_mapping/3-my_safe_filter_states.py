3f4487cb6d741d68ac220fab7ff439fad966df16
tennin12 <bassembenyhia12@gmail.com>
2021-08-28 14:36:39 +0100

    fix f3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-""" injection-safe script that takes an argument and displays
-all values in the states table of selected db """
+"""
+Once again, write a script that takes in arguments
+and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that
+is safe from MySQL injections!
+"""
+import sys
+import MySQLdb
 
 
 def main():
-    import MySQLdb
-    import sys
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    cur.execute("""SELECT id,name FROM states where name = %s
+                ORDER by id ASC""", (search,))
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
 
-    username = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state_arg = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        password=passwd,
-        db=db_name)
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
-    ORDER BY id ASC",  (state_arg, ))
-    state = cur.fetchone()
-    while (state):
-        print(state)
-        state = cur.fetchone()
 
 if __name__ == "__main__":
     main()
