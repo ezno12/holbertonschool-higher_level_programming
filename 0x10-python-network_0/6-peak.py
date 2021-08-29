3742c945aedbb7cf2fce156d32a0d67ede1141dc
tennin12 <bassembenyhia12@gmail.com>
2021-08-29 15:11:11 +0100

    f6 added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 find the peak in unsorted list of integer
 time complexity: O(log(n))
@@ -6,15 +7,5 @@ space complexity: O(1)
 
 
 def find_peak(list_of_integers):
-    start, end = 0, len(list_of_integers) - 1
-    while start < end:
-        mid = start + (end - start) // 2
-
-        # Check if it's a peak
-        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
-            return list_of_integers[mid]
-        if list_of_integers[mid - 1] > list_of_integers[mid + 1]:
-            end = mid
-        else:
-            start = mid + 1
-        return list_of_integers[start]
+    peak = sorted(list_of_integers)[-2]
+    return peak
