f11cfab1c3249f5b3abef47850c0d579277fc321
tennin12 <bassembenyhia12@gmail.com>
2021-09-14 19:16:57 +0100

    f1 added

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+ // writes a file to a string
+const args = process.argv.slice(2);
+const fs = require('fs');
+fs.writeFile(args[0], args[1], 'utf8', function(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+
+    }
+});
