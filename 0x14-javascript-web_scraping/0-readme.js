e8f91f40c490d156925f1c45eb91a84eefbc13bd
tennin12 <bassembenyhia12@gmail.com>
2021-09-14 19:16:49 +0100

    f0 added

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+fs.readFile(args[0], 'utf8', function(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
