86c64a1b9964f4ae578d1901710e50ffc324c789
tennin12 <bassembenyhia12@gmail.com>
2021-09-15 10:11:33 +0100

    f3 fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
 const request = require('request');
-const base = 'https://swapi.co/api/films/' + args[0];
-request.get(base, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(response.body).title);
-  }
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
 });
