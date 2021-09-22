dd467e12dd2273181f535caf64082f5ee1088804
tennin12 <bassembenyhia12@gmail.com>
2021-09-22 20:56:41 +0100

    f2 added

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header
+
+$('DIV#red_header').on('click', function () {
+  $('HEADER').css('color', '#FF0000');
+});
