36afafbf33267806232673aaf2f6d2c6c6220e81
tennin12 <bassembenyhia12@gmail.com>
2021-09-22 21:01:39 +0100

    f100 added

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// updates the text color of the HTML tag HEADER to red (#FF0000)
+// You must use document.querySelector to select the HTML tag
+// You can't use the jQuery API
+// You script must be work when it imported from the HEAD tag
+
+window.onload = function () {
+  document.querySelector('header').style.color = 'red';
+};
